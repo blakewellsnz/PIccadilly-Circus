064e78ca13eeba44d16660e8f21fc5db78776b44
blakewellsnz <blakewellsnz@gmail.com>
2015-07-15 00:54:49 +0000

    Updated config file

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php 
 $currency = '$';
-$db_username = 'ab58098_admin';
-$db_password = 'Ch4ng3m3#';
-$db_name = 'ab58098_piccadilly_circus';
-$db_host = 'localhost';
+$db_username = getenv('C9_USER');
+$db_password = '';
+$db_name = 'piccadilly';
+$db_host = getenv('IP');
 $mysqli = new mysqli($db_host, $db_username, $db_password, $db_name);
 
 ?>
